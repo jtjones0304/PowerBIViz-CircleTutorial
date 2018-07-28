6f3075aaa6a436b35336688f6470bc899090ee23
jtjones0304 <jtjones0304@gmail.com>
2018-07-28 09:06:03 -0700

    Initial Files

---
+++ jeffViz/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var jeffViz986A9862BBBB4268A6EB86563089FB71_DEBUG = {
+        name: 'jeffViz986A9862BBBB4268A6EB86563089FB71_DEBUG',
+        displayName: 'Jeff Viz',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.13.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.jeffViz986A9862BBBB4268A6EB86563089FB71.Visual(options),
+        custom: true
+    };
+}
